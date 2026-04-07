efbf8168b5b39e79804b587895dfea11128a4993
fishySnack <thansu2020@gmail.com>
2026-04-07 15:41:49 -0500

    linked html/css to main

---
+++ src/main/java/com/showly/social_media/Controller/MainPage.java
@@ -0,0 +1,13 @@
+package com.showly.social_media.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainPage {
+    
+    @GetMapping("/")
+    public String mainPage() {
+        return "profile";
+    }
+}
